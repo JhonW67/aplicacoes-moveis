ede103f39e6a1173c21c01803a3c068bd090c67a
João Vitor <joaoocosta161@gmail.com>
2026-05-14 09:08:01 -0400

    remove: projeto1 folder

--- Projeto1/scr/feat/calcularIMC.ts
+++
@@ -1,7 +0,0 @@
-export function calcularIMC(peso: number, altura: number): number {
-  return peso / (altura * altura);
-}
-
-//Camel Case - calcularIMC
-//Pascal Case - CalcularIMC
-//Snake Case - calcular_imc
